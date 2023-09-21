a9b63fa9cc276c9a3d847745b86feb69ac8a5ff8
Dikte Straadt <au611785@uni.au.dk>
2023-09-21 13:14:15 +0200

    code for checking data in each column in visitation

--- Data/read_visitations.py
+++ Data/read_visitations.py
@@ -2,13 +2,11 @@ import pandas as pd
 
 # Import data
 data = pd.read_excel("C:/Users/User/Downloads/Master_Excel_Sep4.xlsx")
-n = len(data)
 
 # List of list of visitations
 vistation_headers = []
 list_list_visitations = []
 
-headers_patient_info = ('ID', 'overall TMJ involvement')
 headers_visitation_0 = ('first_visitation', 'Alder_ved_afslut')
 headers_visitation_1 = ('second_US', 'transversal1')
 headers_visitation_2 = ('third_US', 'transversal2')
@@ -26,8 +24,10 @@ headers_visitation_13 = ('fourteenth_US', 'transversal13')
 headers_visitation_14 = ('fifteenth_US', 'transversal14')
 headers_visitation_15 = ('sixteenth_US', 'transversal15')
 headers_visitation_16 = ('seventeenth_US', 'transversal16')
+headers_patient_info_first = ('ID', 'overall TMJ involvement')
+headers_patient_info_last = ('Dec-11', 'DysfTMJ_Pain_during_palpation')
 
-vistation_headers.extend([headers_patient_info, headers_visitation_0, headers_visitation_1, headers_visitation_2, headers_visitation_3, headers_visitation_4, headers_visitation_5, headers_visitation_6, headers_visitation_7, headers_visitation_8, headers_visitation_9, headers_visitation_10, headers_visitation_11, headers_visitation_12, headers_visitation_13, headers_visitation_14, headers_visitation_15, headers_visitation_16])
+vistation_headers.extend([headers_visitation_0, headers_visitation_1, headers_visitation_2, headers_visitation_3, headers_visitation_4, headers_visitation_5, headers_visitation_6, headers_visitation_7, headers_visitation_8, headers_visitation_9, headers_visitation_10, headers_visitation_11, headers_visitation_12, headers_visitation_13, headers_visitation_14, headers_visitation_15, headers_visitation_16, headers_patient_info_first, headers_patient_info_last])
 
 for visitation in vistation_headers:
     start_index = data.columns.get_loc(visitation[0])
@@ -35,4 +35,3 @@ for visitation in vistation_headers:
     list_visitations = data.iloc[:, start_index : end_index + 1]
     list_list_visitations.append(list_visitations)
 
-print("Done")
