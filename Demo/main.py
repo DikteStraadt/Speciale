41ab929dd2b53757cfd29c8c09a8b4319ac0a566
Dikte Straadt <au611785@uni.au.dk>
2023-12-13 10:01:09 +0100

    ændringer

--- Demo/main.py
+++ Demo/main.py
@@ -5,23 +5,36 @@ import pandas as pd
 
 if __name__ == '__main__':
 
-    # Define data
-    feature_names = ['pain', 'painmove', 'morningstiffness', 'muscularpain', 'headache', 'openingfunction', 'neckpain', 'neckpalpation', 'laterpalp', 'postpalp', 'translation', 'masseter', 'temporalis', 'ptext', 'ptint', 'tempsen', 'sterno', 'stylomandibularligament', 'asymmetrymasseter', 'openingmm', 'opening', 'protrusionmm', 'protrusion', 'laterotrusionrightmm', 'laterotrusionleftmm', 'overjet', 'overbite', 'openbite', 'asybasis', 'asypupilline', 'Biologics', 'Conventional', 'Eye medicine', 'NSAID', 'Corticosteroids', 'profile', 'lowerface', 'asymenton', 'asyoccl', 'asylowmi', 'sagittalrelation']
-
-    #data = np.array([[]])
-    #data = np.array([[0, 0, 0, 0, 0, 0,	0, 0, 0, 0,	0, 0, 0, 0,	0, 0, 0, 0,	0, 0.082637859,	-0.063785877, 1.190879592, -0.13850578,	-0.170677257, -0.448022499,	-0.597958091, -0.072312883,	-0.152292754, -0.002298575,	-0.01179835, 0,	0, 0, 1, 0,	0.022425484, -0.037518222, 0.03799998, 0.023771171,	-0.005597177, 0.010628852]])  # 0
-    #data = np.array([[0, 0, 0, 1, 2, 1, 1, 0, 1, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, -0.921400051, -0.063785877, -1.815357723, -0.13850578, -2.151531469, -2.071441814, -1.622347427, -0.296327696, -0.152292754, -0.002298575, -0.01179835, 1, 1, 0, 1, 1, 0.022425484, -0.037518222, 0.03799998, 0.023771171, -0.005597177, 0.010628852]])  # 1
-    #data = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.623273657, -0.063785877, 0.990931486, -0.13850578, 0.395281089, 0.363687158, 1.450820582, -0.072312883, -0.152292754, -0.002298575, -0.01179835, 1, 0, 0, 1, 0, 0.022425484, -0.037518222, 0.03799998, 0.023771171, -0.005597177, 0.010628852]])  # 0
-    #data = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0.685060605, -0.063785877, -0.612161225, -0.13850578, 0.395281089, -0.177452614, -0.085763423, -0.072312883, -0.152292754, -0.002298575, -0.01179835, 0, 1, 0, 1, 0, 0.022425484, -0.037518222, 0.03799998, 0.023771171, -0.005597177, 0.010628852]]) # 1
-    data = np.array([[1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, -2.697774814, -0.063785877, 0.366532137, -0.13850578, 0.112301916, -0.177452614, -1.110152759, -0.18432029, -0.152292754, -0.002298575, -0.01179835, 0, 0, 0, 1, 0, 0.014077809, -0.037518222, 0.03799998, 0.023771171, -0.005597177, 0.010628852]])  # 1
-
-    visitation = pd.DataFrame(data, columns=feature_names)
-
     # Load model
     with open("test_model.pkl", 'rb') as file:
         model = pickle.load(file)
 
+    # Get feature names
+    feature_names = model.feature_names_in_
+    feature_names = np.concatenate((feature_names, ['ID']))
+
+    # Load data
+    data = pd.read_excel("data.xlsx", "Sheet1")
+
+    # Select feature names columns
+    data = data[feature_names]
+
+    visitation_1 = data[data['ID'] == '6076t_4']
+    visitation_1 = visitation_1.drop('ID', axis=1)
+
+    visitation_2 = data[data['ID'] == '8400a_4']
+    visitation_2 = visitation_2.drop('ID', axis=1)
+
+    visitation_3 = data[data['ID'] == '8843z_4']
+    visitation_3 = visitation_3.drop('ID', axis=1)
+
     # Predict visitation
-    predicted_involvement_status = model.predict(visitation)
-    print(predicted_involvement_status)
+    predicted_involvement_status_1 = model.predict(visitation_1)
+    print(predicted_involvement_status_1)
+
+    predicted_involvement_status_2 = model.predict(visitation_2)
+    print(predicted_involvement_status_2)
+
+    predicted_involvement_status_3 = model.predict(visitation_3)
+    print(predicted_involvement_status_3)
 
